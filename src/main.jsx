459d8f40532235fa69d629c03ecb34d9e12a21c7
Taofeeqoh24 <taofeeqohrabbit20@gmail.com>
2025-06-15 22:24:05 +0100

    Add screenshots and demo GIF

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './app';
 import TodoDetail from './components/tododetail';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
